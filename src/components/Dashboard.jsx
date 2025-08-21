95cb476c252af5c6016e985fb2aa5994a91dbd90
Ruitao Chen <ruitaocchen@gmail.com>
2025-08-21 19:53:38 -0400

    first styling commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <>
+        <div className='dashboard'>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard
